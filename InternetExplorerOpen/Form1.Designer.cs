f5a723ee35d08d4fd98bce5d18c046ff5cfc57f4
huseyinkucuk82 <huseyinkucuk82@hotmail.com>
2020-08-19 16:30:50 +0300

    Add project files.

---
+++ InternetExplorerOpen/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace InternetExplorerOpen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.pnlOrta = new System.Windows.Forms.Panel();
+            this.tbAdress = new System.Windows.Forms.TextBox();
+            this.pnlAlt = new System.Windows.Forms.Panel();
+            this.pnlAlt.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(23, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pnlOrta
+            // 
+            this.pnlOrta.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlOrta.Location = new System.Drawing.Point(0, 22);
+            this.pnlOrta.Name = "pnlOrta";
+            this.pnlOrta.Size = new System.Drawing.Size(800, 371);
+            this.pnlOrta.TabIndex = 1;
+            // 
+            // tbAdress
+            // 
+            this.tbAdress.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tbAdress.Location = new System.Drawing.Point(0, 0);
+            this.tbAdress.Name = "tbAdress";
+            this.tbAdress.Size = new System.Drawing.Size(800, 22);
+            this.tbAdress.TabIndex = 2;
+            // 
+            // pnlAlt
+            // 
+            this.pnlAlt.Controls.Add(this.button1);
+            this.pnlAlt.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.pnlAlt.Location = new System.Drawing.Point(0, 393);
+            this.pnlAlt.Name = "pnlAlt";
+            this.pnlAlt.Size = new System.Drawing.Size(800, 57);
+            this.pnlAlt.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlOrta);
+            this.Controls.Add(this.tbAdress);
+            this.Controls.Add(this.pnlAlt);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.pnlAlt.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel pnlOrta;
+        private System.Windows.Forms.TextBox tbAdress;
+        private System.Windows.Forms.Panel pnlAlt;
+    }
+}
+
